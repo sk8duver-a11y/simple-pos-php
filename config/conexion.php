592ae19c19b22a69f505f157fc9b9ae5797f754f
Duver <sk8duver@gmail.com>
2026-07-23 15:02:36 -0500

    Remove Railway configuration

--- config/conexion.php
+++ config/conexion.php
@@ -1,24 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-// Si existen las variables de Railway, usarlas.
-// De lo contrario, usar la configuración local (XAMPP).
-$servidor = getenv("MYSQLHOST") ?: "localhost";
-$usuario = getenv("MYSQLUSER") ?: "root";
-$contraseña = getenv("MYSQLPASSWORD") ?: "";
-$basedatos = getenv("MYSQLDATABASE") ?: "punto_venta_php";
-$puerto = getenv("MYSQLPORT") ?: 3306;
+$servidor = "localhost";
+$usuario = "root";
+$contraseña = "";
+$basedatos = "punto_venta_php";
 
-$conn = new mysqli(
-    $servidor,
-    $usuario,
-    $contraseña,
-    $basedatos,
-    $puerto
-);
+$conn = new mysqli($servidor, $usuario, $contraseña, $basedatos);
 
-$conn->set_charset("utf8mb4");
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
