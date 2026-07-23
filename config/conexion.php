9d4dd06a180924a2c87f09c20b2b84c0308afd5a
Duver <sk8duver@gmail.com>
2026-07-23 10:44:07 -0500

    Feat: agregar soporte para variables de entorno en Railway

--- config/conexion.php
+++ config/conexion.php
@@ -2,13 +2,20 @@
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$servidor = "localhost";
-$usuario = "root";
-$contraseña = "";
-$basedatos = "punto_venta_php";
+// Si existen las variables de Railway, usarlas.
+// De lo contrario, usar la configuración local (XAMPP).
+$servidor = $_ENV["MYSQLHOST"] ?? "localhost";
+$usuario = $_ENV["MYSQLUSER"] ?? "root";
+$contraseña = $_ENV["MYSQLPASSWORD"] ?? "";
+$basedatos = $_ENV["MYSQLDATABASE"] ?? "punto_venta_php";
+$puerto = $_ENV["MYSQLPORT"] ?? 3306;
 
-$conn = new mysqli($servidor, $usuario, $contraseña, $basedatos);
+$conn = new mysqli(
+    $servidor,
+    $usuario,
+    $contraseña,
+    $basedatos,
+    $puerto
+);
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
+$conn->set_charset("utf8mb4");
