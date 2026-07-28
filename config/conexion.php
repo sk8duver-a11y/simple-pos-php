ce8ca7d21e8d70455600f0ead4df18690437e0a0
Duver <sk8duver@gmail.com>
2026-07-28 15:18:29 -0500

    Crear conexión para TiDB y agregar Dockerfile para Rende

--- config/conexion.php
+++ config/conexion.php
@@ -2,13 +2,31 @@
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$servidor = "localhost";
-$usuario = "root";
-$contraseña = "";
-$basedatos = "punto_venta_php";
+$servidor = getenv('DB_HOST') ?: 'localhost';
+$usuario = getenv('DB_USER') ?: 'root';
+$contraseña = getenv('DB_PASSWORD') ?: '';
+$basedatos = getenv('DB_NAME') ?: 'punto_venta_php';
+$puerto = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($servidor, $usuario, $contraseña, $basedatos);
+$conn = mysqli_init();
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+if (getenv('DB_HOST')) {
+    mysqli_ssl_set(
+        $conn,
+        null,
+        null,
+        null,
+        null,
+        null
+    );
 }
+
+$conn->real_connect(
+    $servidor,
+    $usuario,
+    $contraseña,
+    $basedatos,
+    $puerto
+);
+
+$conn->set_charset("utf8mb4");
